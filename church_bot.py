bb1c351e2edacc4df06401bdc21899c1f5e4138b
Melumi <80072896+Melumi11@users.noreply.github.com>
2021-03-18 11:54:09 -0400

    Update church_bot.py
    generate random rax quotes

--- church_bot.py
+++ church_bot.py
@@ -1,5 +1,5 @@
 #-------------------------------import----------------------------------#
-import discord, asyncio, datetime
+import discord, asyncio, datetime, random
 #-----------------------------------------------------------------------#
 
 
@@ -7,6 +7,7 @@ import discord, asyncio, datetime
 client = discord.Client()
 TOKEN = 'ODIyMTIwOTMyOTQ0MTE3Nzcw.YFNpyA.WTpHSioOjiAPw8OmKEo_LM2xQFU'
 CHAT_ID = 777617289519824949 # Text Channel ID
+quotes = ["I'd never name a baby 'mommy milkers'", "People don’t think it be like it is, but it do.", "You’ll always be my little pogchamp", "Everyone asks if they’re my little pogchamp, but no one ever asks how their server dad is doing", "You're walking in the Caledon Fairgrounds\nThere's no one around and your phone is dead\nOut of the corner of your eye you spot him:\nKan Gao.\nHe's following you, about 30 mooses back.\nHe gets down on all fours and starts shouting probably copyrighted attack warcries\nHe's galloping at you.\nKan Gao.\nYou're looking for the World’s Smallest Ferris Wheel but you're all turned around\nHe's almost upon you now\nAnd you can see there's blood on his face\nMy God, there's tentacles too!\nRunning for your life (from Kan Gao)\nHe's brandishing long cat (holding a knife)\nCthulhu tentacles spreading out (from Kan Gao)\nIndie Game developer Kan Gao", "In 2003, I was camping in the Chilean mountains. After an Ayahuasan ceremony, a Canadian group camped near by. A man, who I had never met before, snuck into my tent mid-trip and made me write out my hallucinations on alpaca wool, using macaroni, Capri-Suns, and raccoon feces. His name was Kan Gao and my name is Quincy.", "Breasts are temporary. Rome is forever.", "Nothing wrong with me loving Moony's mommy milkers", "Pure-chan...\nNyaaa", "I want Pure to take a picture of my crotch.", "Mommy milkers", "*lights a cigarette*", "I’ll show you true pain.", "If humans are innately good, why aren’t all shopping carts returned?", "The worst part of growing up is when no one gets excited if you poop in the toilet."]
 #-----------------------------------------------------------------------#
 
 #Sends a message to CHAT_ID
@@ -14,11 +15,11 @@ async def sendMessage(message):
     await client.get_channel(CHAT_ID).send(message)
 
 #Is called once per asyncio.sleep(seconds)
-async def time():
-    await client.wait_until_ready()
-    while 1:
-        await sendMessage("Tell your pals about this place!")
-        await asyncio.sleep(300)
+#async def time():
+#    await client.wait_until_ready()
+#    while 1:
+#        await sendMessage("Tell your pals about this place!")
+#        await asyncio.sleep(300)
 
 #@client.event
 #async def on_ready():
@@ -32,13 +33,14 @@ async def on_message(message):
         return
 
     if 'raxus' in message.content.lower():
-        await sendMessage("*Church*")
+        await sendMessage("Our Lord :pray:")
 
     if message.content.lower() == '!quote':
-        await sendMessage("Quote placeholder")
+        await sendMessage(quote[random.randint(0, len(quote)])
 
     if 'web' in message.content.lower():
         await sendMessage("https://raxus-church.ml")
+    
     
 client.loop.create_task(time()) # Make needed loop
 client.run(TOKEN) # Launch bot
